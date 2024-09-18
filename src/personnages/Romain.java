3a1f264ed0189420c925949c7725d379bf4ea9aa
Thomas Saurel <srt4886a@ensfsi.onmicrosoft.com>
2024-09-18 14:45:35 +0200

    Classe Romain finie

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -12,7 +12,19 @@ public class Romain {
 	}
 	
 	public void parler(String texte) {
-		
+		System.out.println(prendreParole() + "« " + texte + "»");
 	}
 	
+	private String prendreParole() {
+		return "Le romain " + nom + " : ";
+	}
+	
+	public void recevoirCoup(int forceCoup) {
+		force -= forceCoup;
+		if (force > 0) {
+			parler("Aïe");
+		} else {
+			parler("J'abandonne...");
+		}
+	}
 }
