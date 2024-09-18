bafd5c2d27632ee85b3602f1748a0815cc2cdb1b
Thomas Saurel <srt4886a@ensfsi.onmicrosoft.com>
2024-09-18 14:38:40 +0200

    Classe Gaulois finie

---
+++ src/personnages/Gaulois.java
@@ -0,0 +1,39 @@
+package personnages;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion = 1;
+
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "« " + texte + "»");
+	}
+
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+	}
+
+	/*public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " 
+	+ romain.getNom());
+		romain.recevoirCoup(force / 3);
+	}*/
+
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+	}
+
+	public static void main(String[] args) {
+	}
+
+}
---
+++ src/personnages/Romain.java
@@ -0,0 +1,18 @@
+package personnages;
+
+public class Romain {
+	private String nom;
+	private int force;
+	public Romain(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+	public String getNom() {
+		return nom;
+	}
+	
+	public void parler(String texte) {
+		
+	}
+	
+}
